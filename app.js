53147bebbe92ffd0bd251262920f9c33b9d4b3dc
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-01-13 10:54:31 +0500

    add

--- app.js
+++ app.js
@@ -1,28 +1,83 @@
 
-var display = "";
-function num7(value){
+var operator = ["+", "-", "*", "/"];
 
-   display += value;
-   document.getElementById("screen").value = display;
-   
 
-    
-    
-
-}
+var display = document.getElementById("screen");
 function numRemove(){
 
-    display = "";
-    document.getElementById("screen").value = display;
-    
-     
-     
- 
- }
+   display.value = "";
+  
+}
 
- function cal(){
-    if (num7(value) == "+"){
-        display = num7 + num7 ;
-        document.getElementById("screen").value = display; 
-    }
- }
+function numPer(){
+   display.value += "%"; 
+}
+
+function numDivide(){
+   display.value += "/"; 
+}
+
+function num7(){
+   display.value += "7"; 
+}
+
+function num8(){
+   display.value += "8"; 
+}
+
+
+function num9(){
+   display.value += "9"; 
+}
+
+
+function numMul(){
+   display.value += "*"; 
+}
+
+function num4(){
+   display.value += "4"; 
+}
+
+function num5(){
+   display.value += "5"; 
+}
+
+function num6(){
+   display.value += "6"; 
+}
+
+function numMinus(){
+   display.value += "-"; 
+}
+
+function num1(){
+   display.value += "1"; 
+}
+
+function num2(){
+   display.value += "2"; 
+}
+
+function num3(){
+   display.value += "3"; 
+}
+
+function numAdd(){
+   display.value += "+"; 
+}
+
+function num0(){
+   display.value += "0"; 
+}
+
+function numPoint(){
+   display.value += "."; 
+}
+
+function cal(){
+   if(operator === numAdd){
+    display.value = display.value + display.value;    
+ 
+   }
+}
